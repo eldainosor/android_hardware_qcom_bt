0ef423cc222335311fc8a71aee3d9c14843b1375
ElDainosor <eldainosor@gmail.com>
2017-09-04 16:27:36 +0000

    Revert "bt-caf: Add guard makefile"
    This reverts commit fcc9a3e39fd2a4254c6af4b3fc9e0f77853c6b70.

--- Android.mk
+++ Android.mk
@@ -1,7 +1,3 @@
-BT_VENDOR_PATH := $(call my-dir)
-
-ifeq ($(BT_VENDOR_PATH),$(call project-path-for,bt-vendor))
-
+ifeq ($(call is-vendor-board-platform,QCOM),true)
 include $(call all-named-subdir-makefiles,libbt-vendor)
-
-endif
+endif # is-vendor-board-platform
